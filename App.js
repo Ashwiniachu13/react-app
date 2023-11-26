e20427f9c688e0e561406b7a37535d48af0548df
Ashwini <145188113+Ashwiniachu13@users.noreply.github.com>
2023-11-26 18:08:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import ImageComponent from "./components/images/class/img"
+import ImageImg from "./components/images/functional/image"
+import ListComponent from "./components/list/class/list"
+import ListList from "./components/list/functional/listt"
+import TableComponent from "./components/table/class/table"
+import TableTable from "./components/table/functional/tablee"
+
+
+
+function hey(){
+  return(
+    <div>
+ <TableComponent/>
+ <TableTable/>
+ <ImageComponent/>
+ <ImageImg/>
+ <ListComponent/>
+ <ListList/>
+ 
+ 
+ 
+ </div>
+  )
+}
+export default hey
+
